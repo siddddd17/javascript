40a26195e5ff5a2e593999c7383d7cb678ffaf95
Siddharth Ajith <63159870+siddddd17@users.noreply.github.com>
2023-09-23 22:08:55 +0530

    Change the higher limit value of rgb range

--- learning/color-changing-rectangle/script.js
+++ learning/color-changing-rectangle/script.js
@@ -4,14 +4,15 @@ rect.addEventListener('mousemove', function(event){
     const rectLocation=rect.getBoundingClientRect();
     let cursorLocation=event.clientX-rectLocation.left;
     if(cursorLocation<(rectLocation.width/2)){
-        let redColor=gsap.utils.mapRange(0,rectLocation.width/2,250,0,cursorLocation);
+        let redColor=gsap.utils.mapRange(0,rectLocation.width/2,255,0,cursorLocation);
         gsap.to(rect,{
             color: `rgb(${redColor},0,0)`,
             ease:Power4
         });
+        console.log(rectLocation.width/2);    
     }
     if(cursorLocation>(rectLocation.width/2)){
-        let blueColor=gsap.utils.mapRange(rectLocation.width/2,rectLocation.width,0,250,cursorLocation);
+        let blueColor=gsap.utils.mapRange(rectLocation.width/2,rectLocation.width,0,255,cursorLocation);
         gsap.to(rect,{
             backgroundColor:  `0,0,rgb(${blueColor})`,
             ease:Power4
@@ -20,7 +21,7 @@ rect.addEventListener('mousemove', function(event){
 });
 rect.addEventListener('mouseleave',function(){
     gsap.to(rect,{
-        backgroundColor: white
+        backgroundColor: 'white'
     });
 });
 
