42a0c5089d0a036aa32c4cf3db0df927dbfc3d05
Siddharth Ajith <63159870+siddddd17@users.noreply.github.com>
2023-09-23 18:40:32 +0530

    Add lines to make rectangle white when user moves cursor outside it

--- learning/color-changing-rectangle/script.js
+++ learning/color-changing-rectangle/script.js
@@ -4,17 +4,23 @@ rect.addEventListener('mousemove', function(event){
     const rectLocation=rect.getBoundingClientRect();
     let cursorLocation=event.clientX-rectLocation.left;
     if(cursorLocation<(rectLocation.width/2)){
-        let redColor=gsap.utils.mapRange(0,200,250,0,cursorLocation);
+        let redColor=gsap.utils.mapRange(0,rectLocation.width/2,250,0,cursorLocation);
         gsap.to(rect,{
-            color:  `rgb(${redColor},0,0)`,
+            color: `rgb(${redColor},0,0)`,
             ease:Power4
         });
     }
     if(cursorLocation>(rectLocation.width/2)){
-        let blueColor=gsap.utils.mapRange(200,400,0,250,cursorLocation);
+        let blueColor=gsap.utils.mapRange(rectLocation.width/2,rectLocation.width,0,250,cursorLocation);
         gsap.to(rect,{
-            color:  `0,0,rgb(${blueColor})`,
+            backgroundColor:  `0,0,rgb(${blueColor})`,
             ease:Power4
         });
     }
 });
+rect.addEventListener('mouseleave',function(){
+    gsap.to(rect,{
+        backgroundColor: white
+    });
+});
+
